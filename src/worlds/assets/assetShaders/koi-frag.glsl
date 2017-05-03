a30c03025b7f48f69365a6d9fb922da7474d3ffc
andreahlin <andrea.lin96@yahoo.com>
2017-05-03 00:02:17 -0400

    procedurally colored koi

--- src/worlds/assets/assetShaders/koi-frag.glsl
+++ src/worlds/assets/assetShaders/koi-frag.glsl
@@ -5,6 +5,13 @@ varying float noise;
 uniform float u_time; 
 uniform vec3 light_vec; 
 
+// noise function 
+float noiseValue(float x, float y, float z){
+  float value1 = fract(sin(dot(vec2(z, y) ,vec2(1027.9898, 29381.233))) * 333019.5453);
+  float value2 = fract(sin(x) * 43758.5453);
+  return dot(value1, value2); 
+}
+
 // cosine based palette from IQ 
 vec3 palette( in float t, in vec3 a, in vec3 b, in vec3 c, in vec3 d )
 {
@@ -12,14 +19,15 @@ vec3 palette( in float t, in vec3 a, in vec3 b, in vec3 c, in vec3 d )
 }
 
 void main() {
-
     // compute colors
     float speed = 0.1; 
     vec3 col = palette(speed * u_time, vec3(0.5,0.5,0.5),vec3(0.5,0.5,0.5),vec3(1.0,0.7,0.4),vec3(0.0,0.15,0.20) );
 
   vec2 uv = vec2(1,1) - vUv;
   vec3 pink = vec3(0.898, 0.682, 0.866);
-  vec3 dGreen = vec3(0.109, 0.360, 0.078); 
+  float randColR = noiseValue(f_position.x, f_position.y, f_position.z);
+
+  vec3 randCol = vec3(randColR,0.682 + randColR / 4.0, 0.866 + randColR / 2.0); 
 
   // hardcoded light vector 
   // vec3 light_vec = vec3(1.0, 1.0, 2.0); 
@@ -29,5 +37,5 @@ void main() {
   float globalIllum = 0.2; 
 
   // out color
-  gl_FragColor = vec4( lambert * pink, 1.0) + globalIllum * vec4(pink, 1.0);
+  gl_FragColor = vec4( lambert * randCol, 1.0) + globalIllum * vec4(pink, 1.0);
 }
